e6ae70ce50b474a55c4a43001efc4dfe6186b351
Jeremy Grubard <jgrubard@gmail.com>
2018-01-28 13:38:32 -0500

    full functionality

--- server.js
+++ server.js
@@ -1,60 +1,43 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+const path = require('path');
 const nunjucks = require('nunjucks');
 nunjucks.configure({ noCache: true });
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
-app.get('/tweets', (req, res, next) => {
-  db.getTweets((err, tweets) => {
-    if (err) return next(err);
-    res.send(tweets);
-  });
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
+
+app.use((req, res, next) => {
+  res.locals.path = req.url;
+  next();
 });
 
-app.get('/tweets/:id', (req, res, next) => {
-  db.getTweet(req.params.id, (err, tweet) => {
-    if (err) return next(err);
-    res.send(tweet);
-  });
+app.get('/', (req, res, next) => {
+  res.render('index', { title: 'Twitter' });
 });
 
-// app.get('/', (req, res, next) => {
-//   res.render('index', { title: 'Home' });
-// });
-
-// app.get('/tweets', (req, res, next) => {
-//   res.render('tweets', { title: 'Tweets' });
-// });
+app.use('/tweets', require('./routes/tweets'));
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`Listening on Port ${port}`);
 });
 
-
 db.sync((err) => {
   if (err) return console.log(err);
-  console.log('tables have been created!');
   db.getTweets((err, tweets) => {
     if (err) return console.log(err);
-    console.log(`there are ${tweets.length} users`);
     db.seed((err) => {
       if (err) return console.log(err);
-      console.log('seeded');
       db.getTweets((err, tweets) => {
         if (err) return console.log(err);
-        console.log(`there are ${tweets.length} users`);
         db.getTweet(3, (err, tweet) => {
           if(err) return console.log(err);
-          console.log(`tweet with and id of 3 is ${tweet.tweet}`);
         });
       });
     });
   });
-
-
 });
